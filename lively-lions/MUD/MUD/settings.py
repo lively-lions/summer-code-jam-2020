80181a9f67d5b99646f6f023be982e059a16d4fe
Zorakinezear <alexaderviera@gmail.com>
2020-08-06 02:25:55 -0700

    second test

--- lively-lions/MUD/MUD/settings.py
+++ lively-lions/MUD/MUD/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'objects.apps.ObjectsConfig',
+    # 'objects.apps.ObjectsConfig',
 ]
 
 MIDDLEWARE = [
